08833a20077be9832f5384b19c9898d84094ee13
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2022-10-05 17:25:35 +0200

    ex00 done

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 17:01:17 by amuhleth          #+#    #+#             */
+/*   Updated: 2022/10/05 17:24:21 by amuhleth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			std::string str(argv[i]);
+			for (size_t j = 0 ; j < str.size() ; j++)
+				std::cout << (char)std::toupper(str[j]);
+		}
+		std::cout << std::endl;
+	}
+}
