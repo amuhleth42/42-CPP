0f6042624fbafb003791b5bc2c3d765271dfd865
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2022-10-05 17:43:05 +0200

    ex01 wip

---
+++ cpp00/ex01/Contact.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 17:32:11 by amuhleth          #+#    #+#             */
+/*   Updated: 2022/10/05 17:36:38 by amuhleth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+class Contact {
+
+public:
+
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	darkest_secret;
+};
+
+#endif
---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 17:29:47 by amuhleth          #+#    #+#             */
+/*   Updated: 2022/10/05 17:37:43 by amuhleth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+
+class PhoneBook {
+
+public:
+
+	Contact	tab[8];
+};
+
+#endif
