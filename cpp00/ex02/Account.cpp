5becb43f399ef280ad39dd6c906b0735800c792f
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2022-10-11 21:46:13 +0200

    TODO : timestamps

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 20:19:02 by amuhleth          #+#    #+#             */
-/*   Updated: 2022/10/11 21:08:55 by amuhleth         ###   ########.fr       */
+/*   Updated: 2022/10/11 21:43:29 by amuhleth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,7 @@ void	Account::displayAccountsInfos( void )
 	std::cout << "accounts:" << Account::getNbAccounts() << ";";
 	std::cout << "total:" << Account::getTotalAmount() << ";";
 	std::cout << "deposits:" << Account::getNbDeposits() << ";";
-	std::cout << "withdrawals:" << Account::getNbWithdrawals() << ";";
-	std::cout << std::endl;
+	std::cout << "withdrawals:" << Account::getNbWithdrawals() << std::endl;
 }
 
 
@@ -59,6 +58,7 @@ Account::Account( int initial_deposit ) : _amount(initial_deposit)
 	std::cout << "index:" << this->_accountIndex << ";"; 
 	std::cout << "amount:" << this->_amount << ";created";
 	std::cout << std::endl;
+	Account::_totalAmount += this->_amount;
 }
 
 Account::~Account( void )
@@ -71,20 +71,43 @@ Account::~Account( void )
 
 void	Account::makeDeposit( int deposit )
 {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";"; 
+	std::cout << "p_amount:" << this->_amount << ";";
+	
 	this->_nbDeposits++;
 	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+	Account::_totalNbDeposits++;
+
+	std::cout << "deposit:" << deposit << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "nb_deposits:" << this->_nbDeposits << std::endl;
+
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";"; 
+	std::cout << "p_amount:" << this->_amount << ";";
 	if (this->_amount >= withdrawal)
 	{
 		this->_nbWithdrawals++;
 		this->_amount -= withdrawal;
+		Account::_totalAmount -= withdrawal;
+		Account::_totalNbWithdrawals++;
+	
+		std::cout << "withdrawal:" << withdrawal << ";";
+		std::cout << "amount:" << this->_amount << ";";
+		std::cout << "nb_withdrawals:" << this->_nbWithdrawals << std::endl;
 		return (0);
 	}
 	else
+	{
+		std::cout << "withdrawal:refused" << std::endl;
 		return (1);
+	}
 }
 
 int	Account::checkAmount( void ) const
@@ -94,6 +117,11 @@ int	Account::checkAmount( void ) const
 
 void	Account::displayStatus( void ) const
 {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";"; 
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "deposits:" << this->_nbDeposits << ";";
+	std::cout << "withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
 void	Account::_displayTimestamp( void )
--- cpp00/ex02/tests.cpp
+++ cpp00/ex02/tests.cpp
@@ -21,9 +21,7 @@ int		main( void ) {
 
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	// constructeur avec args
 	accounts_t				accounts( amounts, amounts + amounts_size );
-	// constructeurs de copie jcrois
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
 
