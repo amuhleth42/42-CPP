c1e796ed290de5f7f2b2436200f76aa2733f44cb
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2022-10-12 00:43:06 +0200

    cpp01 start

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 00:39:17 by amuhleth          #+#    #+#             */
+/*   Updated: 2022/10/12 00:42:20 by amuhleth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+class Zombie {
+
+public:
+
+	void	announce( void );
+
+private:
+
+	std::string	name;
+
+
+};
+
+#endif
